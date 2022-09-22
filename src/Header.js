8d5355a3b5000359e385e0680df5177478de2b82
devpop <impopcity@gmail.com>
2022-09-22 22:26:53 +0530

    first css apply

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,9 @@
 import React from 'react'
 
 const Header = () => {
-  const headerstyle = {
-    backgroundColor:"pink",
-    color:"white"
-  }
+  
   return (
-    <header style={headerstyle}>
+    <header>
         <h1>Grocieries list</h1>
     </header>
   )

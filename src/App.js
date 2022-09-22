0d6eb5833b2a96d83b60a5c023b79647361a7ed2
devpop <impopcity@gmail.com>
2022-09-22 15:04:22 +0530

    function adding

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const name = "pavitharan";
+  let name1 = "pavitharan"
+  let name2 = "vigneshwaran"
+  let name3 = "navanandhini"
+  const altername = function(){
+    const name = [name1,name2,name3]
+    const int  = Math.floor(Math.random()*3)
+    return name[int];
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HELLO {name}!
+          hello {altername()}!
         </p>
    
         

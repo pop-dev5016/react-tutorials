59710063083e97bc511454de0ef085737eae62e5
devpop <impopcity@gmail.com>
2022-09-23 13:37:46 +0530

    logical commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-<<<<<<< HEAD
-
-
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
-=======
 import Greet from "./component/Function";
->>>>>>> 383f6a6c26d9abf3825ef5bb869029439b0606cc
-
 function App() {
-  
- 
   return (
     <div className="App">
-<<<<<<< HEAD
      <Header/> 
      <Content/>
-     <Footer/>
-=======
-     <Greet/>
-     
->>>>>>> 383f6a6c26d9abf3825ef5bb869029439b0606cc
+     <Footer/>   
     </div>
   );
 }

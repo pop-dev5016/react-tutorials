c4eb4d4ea5d111eb18f993c073a283502a72f39e
devpop <impopcity@gmail.com>
2022-09-23 13:21:03 +0530

    check

--- src/Content.js
+++ src/Content.js
@@ -22,7 +22,8 @@ const content = () => {
      ]);
 
      const handleCheck = (id) =>{
-          const listItems = item.map.item///check
+          const listItems = item.map.item///che
+          cons
           
      }
     

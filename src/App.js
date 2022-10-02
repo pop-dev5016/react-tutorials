d16e4c71ada0bdeb9e82e743d6ec8109b5148da3
devpop <impopcity@gmail.com>
2022-10-02 14:32:23 +0530

    project 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
-import Greet from "./component/Function";
+
+
+
 function App() {
   return (
     <div className="App">
--- src/Content.js
+++ src/Content.js
@@ -1,48 +1,51 @@
-import React from 'react'
-import { useState } from "react"
-import { FaTrashAlt } from "react-icons/fa";
+import React,{ useState } from 'react'
+import {FaTrashAlt} from 'react-icons/fa'
 
-const content = () => {
-     const [items,setItems] = useState([
-        {
-            id:1,
-            checked:false,
-            item:"one half founf of bag of coco for with almound unsalted"
-        },
-        {
-            id:2,
-            checked:true,
-            item:"item 2"
-        },
-        {
-            id:3,
-            checked:false,
-            item:"item 3"
-        }
-     ]);
 
-     const handleCheck = (id) =>{
-          const listItems = item.map
-          cons
-          
-     }
-    
+
+const Content = () => {
+
+    const [items,SetItems] = useState([{id:5,checked:true,item:"one half powered coco paste is found man"},
+    {id:6,checked:false,item:"Item 2"},
+    {id:7,checked:false,item:"Item 3"}])
+
+    const handlecheck = (id)=>{
+        const listItems = items.map((item)=>item.id==id?{...item,checked:!item.checked}:item)
+        SetItems(listItems);
+        localStorage.setItem("shoppinglist",JSON.stringify(listItems));
+    }
+
+    const handledelete = (id)=>{
+        const listItems= items.filter((item)=>item.id !== id);
+        SetItems(listItems);
+        localStorage.setItem("shoppinglist",JSON.stringify(listItems));
+    }
+     
+
         return (
             <main>
+                {items.length?(
                 <ul>
-                    {items.map((item)=>(
-                        <li className='item' key={item.id}>
-                           <input type="checkbox" 
-                            onChange={()=>handleCheck(item.id)} checked={item.checked}/>
-                           <label >{item.item}</label>
-                           <FaTrashAlt role="button" tabIndex="0"/>
-                          
-                        </li>
-                    ))}
-                </ul>
-            
+                            {items.map((item)=>(
+                                <li className='item' key = {item.id}>
+                                    <input type ="checkbox"
+                                    onChange={()=>handlecheck(item.id)}
+                                    checked={item.checked}/>
+
+                                    <label onDoubleClick={()=>handlecheck(item.id)} 
+                                    style={(item.checked)?{textDecoration:"line-through"}:null}> {item.item}</label>
+
+                                    <FaTrashAlt onClick={()=>handledelete(item.id)}
+                                    role="button" tabIndex="0"></FaTrashAlt>
+                                </li>
+                            ))}
+                        </ul>
+                ):(
+                    <p style={{marginTop:"2rem"}}>oops...your list is empty</p>
+                )
+                }
             </main>
         )
   }
 
-export default content
+export default Content
